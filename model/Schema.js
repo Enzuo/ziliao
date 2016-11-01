bb0da518f8cfe7614e4166bdb1e72724c0099496
Enzuo <potc.zone+git@gmail.com>
2016-11-01 17:53:10 +0100

    Add user id filter

--- model/Schema.js
+++ model/Schema.js
@@ -6,19 +6,31 @@ const db = require( './database' )
 const User = require( './User' )
 const Knowledge = require( './Knowledge' )
 
-console.log('Knowledge', Knowledge)
-
 const QueryRoot = new GraphQLObjectType({
 	name: 'Query',
 	fields: () => ({
 		users: {
 			type: new GraphQLList( User ),
+			args: {
+				id: { type: GraphQLString }
+			},
 			resolve: (parent, args, context, resolveInfo) => {
 				return joinMonster( resolveInfo, {}, sql => {
 					console.log( sql )
 					return db.query( sql )
 				})
-			}
+			},
+			where: (usersTable, args, context) => {
+				if (args.id) {
+					let userId = args.id
+					let operator = '='
+					if (userId[ 0 ] === '!') {
+						operator = '!='
+						userId = userId.slice( 1 )
+					}
+					return `${usersTable}.id ${operator} ${userId}`
+				}
+			},
 		},
 		knowledges: {
 			type: new GraphQLList( Knowledge ),
