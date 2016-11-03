c1f502bc607363e1cc507b3cc5cdf9797e06f0c3
Enzuo <potc.zone+git@gmail.com>
2016-11-03 21:45:50 +0100

    Add knowledge mutation

--- model/Schema.js
+++ model/Schema.js
@@ -1,6 +1,6 @@
 const joinMonster = require( 'join-monster' ).default
 const { GraphQLSchema } = require( 'graphql' )
-const { GraphQLObjectType, GraphQLList, GraphQLString, GraphQLInt } = require( 'graphql' )
+const { GraphQLObjectType, GraphQLList, GraphQLString, GraphQLInt, GraphQLNonNull } = require( 'graphql' )
 
 const db = require( './database' )
 const { User } = require( './User' )
@@ -38,16 +38,18 @@ const QueryRoot = new GraphQLObjectType({
 			sqlPaginate: true,
 			sortKey: {
         		order: 'desc',
-        		key: [ 'dateUpdated', 'id' ]
+        		key: ['dateUpdated', 'id']
       		},
 			resolve: (parent, args, context, resolveInfo) => {
 				// console.log("resolve", resolveInfo, args)
-				return joinMonster( resolveInfo, {}, sql => {
+				console.log("resolve")
+				joinMonster( resolveInfo, {}, sql => {
 					console.log( sql )
 					return db.query( sql )
 				}, { dialect : 'pg' })
 			},
 			where: (knowledgeTable, args, context) => {
+				console.log('where')
 				if (args.search) {
 					return `to_tsvector(${knowledgeTable}.problem) @@  to_tsquery('${args.search}')`
 				}
@@ -56,9 +58,50 @@ const QueryRoot = new GraphQLObjectType({
 	})
 })
 
+var count = 0
+
+const MutationRoot = new GraphQLObjectType({
+	name: 'RootMutationType',
+	fields: {
+		addKnowledge: {
+			type: Knowledge,
+			description: 'Create a new Knowledge',
+			args: {
+				problem: { type: new GraphQLNonNull( GraphQLString )},
+				solution: { type: new GraphQLNonNull( GraphQLString )},
+				idAuthor: { type: new GraphQLNonNull( GraphQLInt )}
+			},
+			resolve: function (parent, args, context, resolveInfo) {
+				
+				return db.query(
+					`INSERT INTO "Knowledge" 
+					("problem", "solution", "idAuthor") VALUES 
+					( $1, $2, $3 ) 
+					RETURNING "id"`
+					, [args.problem, args.solution, args.idAuthor])
+				.then( (data) => {
+					if (data.length < 1) throw 'err'
+					var updateContext =  { id : data[0].id }
+					return joinMonster( resolveInfo, updateContext, sql => {
+						console.log( sql )
+						return db.query( sql )
+					})
+				})
+			},
+			where: (knowledgeTable, args, context) => {
+				var _id = context.id
+				if (_id) {
+					return `${knowledgeTable}."id" = ${_id}`
+				}
+			},
+		}
+	}
+})
+
 const Schema = new GraphQLSchema({
 	description: 'a test schema',
-	query: QueryRoot
+	query: QueryRoot,
+	mutation: MutationRoot,
 })
 
 module.exports = Schema
